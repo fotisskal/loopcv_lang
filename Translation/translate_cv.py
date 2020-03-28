b3e304e0c367e109645c5784ceedb57a0b590c39
Fotis Kalathopoulos <kalathopoulos@niometrics.com>
2020-03-28 23:59:12 +0200

    clean

--- Translation/translate_cv.py
+++ Translation/translate_cv.py
@@ -21,33 +21,26 @@ class CV(BaseModel):
     cv: str
 
 
+@app.post("/googletrans")
+async def pygoogletrans(request: CV):
+    return py_google_trans_api(request.language, request.cv)
+
+
 # "QUERY LENGTH LIMIT EXCEEDED. MAX ALLOWED QUERY : 500 CHARS"
 @app.post("/translate")
 async def translate(request: CV):
     return translate_api(request.language, request.cv)
 
 
-# 0.7678499221801758
-@app.post("/googletrans")
-async def pygoogletrans(request: CV):
-    return py_google_trans_api(request.language, request.cv)
+def py_google_trans_api(language, cv):
+    translator = googletrans.Translator()
+    iso_code = list(languages_dict.keys())[list(languages_dict.values()).index(language.lower())]
+    translated_text = translator.translate(cv, dest=iso_code).text
+    return "r'%s'" % translated_text
 
 
 def translate_api(language, cv):
-    start_time = time.time()
     iso_code = list(languages_dict.keys())[list(languages_dict.values()).index(language.lower())]
-    print("Text size: %s" % cv.__len__())
     translator = translate.Translator(to_lang=iso_code)
     translated_text = translator.translate(cv)
-    print(time.time() - start_time)
     return translated_text
-
-
-def py_google_trans_api(language, cv):
-    start_time = time.time()
-    translator = googletrans.Translator()
-    iso_code = list(languages_dict.keys())[list(languages_dict.values()).index(language.lower())]
-    print("Text size: %s" % cv.__len__())
-    translated_text = translator.translate(cv, dest=iso_code).text
-    print(time.time() - start_time)
-    return "r'%s'" % translated_text
