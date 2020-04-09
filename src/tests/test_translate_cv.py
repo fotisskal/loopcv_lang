30be6dbbb0f17d9b45eb96856c032ad643e51fcb
fotisskal <fotis.kal21@gmail.com>
2020-04-09 04:04:40 +0200

    clean

--- src/tests/test_translate_cv.py
+++ src/tests/test_translate_cv.py
@@ -2,14 +2,14 @@ from unittest import TestCase
 
 import googletrans
 
-from Translation.translate_cv import translate_api
+from src.Translation.translate_cv import py_google_trans_api
 
 
 class Test(TestCase):
     def test_translate_api(self):
         language = "French"
         cv = "Read the following text very carefully and see what you can understand without looking at the English translation"
-        print(translate_api(language, cv))
+        print(py_google_trans_api(language, cv))
 
 
 class TestB(TestCase):
